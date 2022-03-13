8f21c86f8e21f80e0f62aeeacfbe74b5bbcaee2d
Hugo NICOLAS-HUMBERT <hugo.nicolas-humbert@akanea.com>
2022-03-13 14:42:45 +0100

    Details

--- PokemonDetails.js
+++ PokemonDetails.js
@@ -1,14 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text} from 'react-native';
+import { StyleSheet, View, Text, ImageBackground, StatusBar, TouchableOpacity} from 'react-native';
 
 class PokemonDetails extends React.Component {
+
+    goToList = () => {
+        this.props.navigation.navigate('PokeDex')
+      }
+
     render() {
         return (
-            <View>
-                <Text>gougoguo</Text>
+            <View style={styles.container}>
+                <ImageBackground source={require('./assets/background.jpg')} style={styles.background}>               
+                <View style={styles.header}>
+                    <TouchableOpacity style={styles.backButton} onPress={() => this.goToList()}>
+                    </TouchableOpacity>
+                </View>
+                </ImageBackground>
             </View>
         )
     }
 }
 
 export default PokemonDetails;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight,
+
+    },
+    backButton: {
+        backgroundColor: "#D3D3D3",
+        justifyContent: 'flex-start',
+        marginVertical: 8,
+        marginHorizontal: 16,
+        padding: 20,
+        flexDirection: 'row',
+      },
+    background: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+  });
